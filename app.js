43639839b3a114159dc3b8c90a34a2fc17dfc40d
AkankshaNannure26 <49149643+AkankshaNannure26@users.noreply.github.com>
2020-08-22 19:27:00 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,123 @@
+var queen =
+{
+    position:
+    {
+        x:0,
+        y:0
+    }
+};
+var board=[["a8","b8","c8","d8","e8","f8","g8"],
+            ["a7","b7","c7","d7","e7","f7","g7"],
+            ["a6","b6","c6","d6","e6","f6","g6"],
+            ["a5","b5","c5","d5","e5","f5","g5"],
+            ["a4","b4","c4","d4","e4","f4","g4"],
+            ["a3","b3","c3","d3","e3","f3","g3"],
+            ["a2","b2","c2","d2","e2","f2","g2"],
+            ["a1","b1","c1","d1","e1","f1","g1"]];
+function move(noofsteps, direction)
+{
+    let temp;
+    let temp1;
+    switch(direction){
+        case "E":temp=queen.position.y+noofsteps;
+            if(temp>8 || temp<0)
+            {
+                console.log("Out of boundary");
+                document.write("Out of boundary");
+            }
+            else{
+                queen.position.y=temp;
+            }
+            break;
+        case "W":temp=queen.position.y-noofsteps;
+            if(temp>8 || temp<0)
+            {
+                console.log("Out of boundary");
+                document.write("Out of boundary");
+            }
+            else{
+                queen.position.y=temp;
+            }
+            break;
+        case "S":temp=queen.position.x+noofsteps;
+            if(temp>8 || temp<0)
+            {
+            console.log("Out of boundary");
+            document.write("Out of boundary");
+            }
+            else{
+            queen.position.x=temp;
+            }
+            break;
+        case "N":temp=queen.position.x-noofsteps;
+            if(temp>8 || temp<0)
+            {
+                console.log("Out of boundary");
+                document.write("Out of boundary");
+            }
+            else{
+                queen.position.x=temp;
+            }
+            break;
+        case "SE":temp=queen.position.x+noofsteps;
+                 temp1=queen.position.y+noofsteps;
+            if((temp>8 || temp<0) && ( temp1>8 || temp<0))
+            {
+                console.log("Out of boundary");
+                document.write("Out of boundary");
+            }
+            else{
+                queen.position.x=temp;
+                queen.position.y=temp1;
+            }
+            break;
+        case "SW":temp=queen.position.x+noofsteps;
+                 temp1=queen.position.y-noofsteps;
+            if((temp>8 || temp<0) && ( temp1>8 || temp<0))
+            {
+                console.log("Out of boundary");
+                document.write("Out of boundary");
+            }
+            else{
+                queen.position.x=temp;
+                queen.position.y=temp1;
+            }
+            break;
+        case "NW":temp=queen.position.x-noofsteps;
+                 temp1=queen.position.y-noofsteps;
+            if((temp>8 || temp<0) && ( temp1>8 || temp<0))
+            {
+                console.log("Out of boundary");
+                document.write("Out of boundary");
+            }
+            else{
+                queen.position.x=temp;
+                queen.position.y=temp1;
+            }
+            break;
+        
+        case "NE":temp=queen.position.x-noofsteps;
+                 temp1=queen.position.y+noofsteps;
+            if((temp>8 || temp<0) && ( temp1>8 || temp<0))
+            {
+                console.log("Out of boundary");
+                document.write("Out of boundary");
+            }
+            else{
+                queen.position.x=temp;
+                queen.position.y=temp1;
+            }
+            break;
+        default: document.write("Invalid position");
+    }
+}
+function play(){
+    var value=document.getElementById("position").value;
+    var input=value.split(" ");
+    input.forEach(num => {
+        let number=Number(num.slice(num.length-1));
+        let direction=num.slice(0,num.length-1);
+        move(number,direction);
+    });
+   console.log("Queens current position ->"+board[queen.position.x][queen.position.y]);
+}
